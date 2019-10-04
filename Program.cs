2e06f41ba5f223a02f8d5e787c47c05112bb09bf
Jonas Brunhage <jonas.brunhage@gmail.com>
2019-10-04 10:54:23 +0200

    Add menu and exit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace CarSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            bool running = true;
+
+            while (running)
+            {
+
+                Console.WriteLine("1. Add car");
+                Console.WriteLine("2. List cars");
+                Console.WriteLine("3. Simulate speed");
+                Console.WriteLine("4. Exit");
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                Console.Clear();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+
+                        break;
+
+                    case ConsoleKey.D4:
+                    case ConsoleKey.NumPad4:
+                        running = false;
+                        break;
+
+                }
+                Console.Clear();
+            }
+
         }
     }
 }
